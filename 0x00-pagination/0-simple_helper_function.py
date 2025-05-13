6a1336f8d7209595b493e4ebee82c6f1538109c0
nathen321 <walidjalaly43@gmail.com>
2025-05-13 15:50:09 +0000

    task 0 cheker

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+""" 0-simple_helper_function module """
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Calculate the start and end index for pagination."""
     if page == 1:
         return (0, page_size)
