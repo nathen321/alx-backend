bf5dbbb2513d2b011d495debc8343d777f60c9a4
nathen321 <walidjalaly43@gmail.com>
2025-05-13 23:56:24 +0000

    task 1 cheker

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import csv
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """Calculate the start and end index for pagination."""
+    if page == 1:
+        return (0, page_size)
+    else:
+        indx = ((page - 1) * page_size)
+        return (indx, indx + page_size)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        data = self.dataset()
+        idex = index_range(page, page_size)
+        return data[idex[0]:idex[1]]
