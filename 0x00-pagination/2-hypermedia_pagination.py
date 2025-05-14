295114d23109755a46f738d4d18af144f77ef17d
nathen321 <walidjalaly43@gmail.com>
2025-05-14 01:04:37 +0000

    task 2 cheker

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""Task 1: Simple pagination.
+"""
+
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """Calculate the start and end index for pagination."""
+    if page == 1:
+        return (0, page_size)
+    else:
+        indx = ((page - 1) * page_size)
+        return (indx, indx + page_size)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get a page of data from the dataset.
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        data = self.dataset()
+        idex = index_range(page, page_size)
+        if idex[0] > len(data):
+            return []
+        return data[idex[0]:idex[1]]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+        total_data = len(self.dataset())
+        total_pages = math.ceil(total_data / page_size) if page_size else 0
+        hyper = {"page_size": page_size, "page": page,
+                 "data": self.get_page(page, page_size),
+                 "next_page": page + 1 if page < total_pages else None,
+                 "prev_page": page - 1 if page > 1 else None,
+                 "total_pages": total_pages}
+        return hyper
