7fd067a139944f1a5e2d744c1f707a43bef022a1
walid jalaly <46631425+nathen321@users.noreply.github.com>
2025-05-15 11:15:30 +0100

    Update 0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -9,10 +9,6 @@ class BasicCache(BaseCaching):
       - constants of your caching system
       - where your data are stored (in a dictionary)
     """
-    def __init__(self):
-        """ Initiliaze
-        """
-        super().__init__()
 
     def put(self, key, item):
         """ Add an item in the cache
