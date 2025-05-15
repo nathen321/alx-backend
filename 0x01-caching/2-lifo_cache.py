1b7cd0290f69cfe58d1e1d4d5d0b93298f364b86
nathen321 <walidjalaly43@gmail.com>
2025-05-15 16:53:05 +0000

    task 2 cheker

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+'''Task 2: Basic dictionary
+'''
+
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    '''A class `BasicCache` that inherits from `BaseCaching`
+       and is a caching system
+    '''
+    def __init__(self):
+        """ initialize
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", last_key)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key in self.cache_data.keys() and key is not None:
+            return self.cache_data[key]
