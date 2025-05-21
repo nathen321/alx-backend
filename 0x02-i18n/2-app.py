3d0efc5048b07e82523d1cddad35e2edf21daecf
walid jalaly <46631425+nathen321@users.noreply.github.com>
2025-05-21 22:15:13 +0100

    feat: Add second Flask app and corresponding HTML template

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -4,6 +4,7 @@
 
 from flask import Flask, render_template
 from flask_babel import Babel
+from flask import request
 
 app = Flask(__name__)
 
@@ -22,7 +23,17 @@ babel = Babel(app)
 @app.route('/')
 def index():
     '''default route'''
-    return render_template('1-index.html')
+    return render_template('2-index.html')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
+
+    Returns:
+        str: best match
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == '__main__':
