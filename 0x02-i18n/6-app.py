ce5c4c7f44b8907611205534a2ce7cbbef5e5e0b
walid jalaly <46631425+nathen321@users.noreply.github.com>
2025-05-22 17:14:55 +0100

    feat: Implement locale selection via URL parameter and enhance homepage template

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+'''Task 6: Force locale with URL parameter
+'''
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+
+class Config:
+    '''Config class'''
+
+    DEBUG = True
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
+
+    Returns:
+        str: best match
+    """
+    user = get_user()
+    if user:
+        if user.get('locale'):
+            return user.get('locale')
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    '''default route
+
+    Returns:
+        html: homepage
+    '''
+    return render_template("6-index.html")
+
+
+@app.before_request
+def before_request():
+    ''' user login system is outside the scope of this project.
+    '''
+    g.user = get_user()
+
+
+def get_user():
+    '''retreave user
+    '''
+    user = request.args.get('login_as')
+    if user:
+        return users.get(int(user))
+    return None
+
+
+if __name__ == '__main__':
+    app.run()
