0b41303d2fd5b039aa933d03c7ad7d41bda386b1
walid jalaly <46631425+nathen321@users.noreply.github.com>
2025-05-22 02:43:08 +0100

    feat: Add initial Flask app and HTML template with internationalization support

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+'''Task 1: Basic Flask app
+'''
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    '''configuration class'''
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    '''default route'''
+    return render_template('4-index.html')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
+
+    Returns:
+        str: best match
+    """
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
